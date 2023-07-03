0ca98b4aee3d48106a6a57f6fca551adca8784ab
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-07-03 11:02:43 +0700

    Add project files.

---
+++ FinalProject_125/FinalProject_125.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+class MataKuliah;
+class presensi {
+	string nilai;
+	nilai (string pNilai) : nilai (pNilai)
+	cout << "Nilai presensi \"" << nama << "\" ada \n"
+
+};
+
+class activity {
+	string nilai;
+	nilai(string pNilai) : nilai(pNilai)
+		cout << "Nilai activity \"" << nama << "\" ada \n"
+
+};
+
+class exercise {
+	string nilai;
+	nilai(string pNilai) : nilai(pNilai)
+		cout << "Nilai exercise \"" << nama << "\" ada \n"
+
+};
+
+class TugasAkhir {
+	string nilai;
+	nilai(string pNilai) : nilai(pNilai)
+		cout << "Nilai matakuliah \"" << nama << "\" ada \n"
+
+};
+
+void presensi::tambah MataKuliah(MataKuliah* pMataKuliah) {
+	daftar_nilai.push_back(pNilai);
+}
+
+void activity::tambah nilai(nilai* pNilai) {
+	daftar_nilai.push_back(pDokter);
+}
+
+void exercise::tambah nilai(nilai* pNilai) {
+	daftar_nilai.push_back(pNilai);
+}
+
+void tugasAkhir::tambah nilai(nilai* pNilai) {
+	daftar_nilai.push_back(pNilai);
+}
+
+
+class Pemrograman : public MataKuliah {
+	//isi disini
+};
+
+int main() {
+
+	//isi disini
+}
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
